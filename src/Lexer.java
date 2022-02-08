0e14ae474ce7231181bfa5612a9ff2ded01edcc1
MasonN808 <mason.nakamura1@marist.edu>
2022-02-07 22:44:24 -0500

    Added tokens for the Lexer

--- src/Lexer.java
+++ src/Lexer.java
@@ -1,7 +1,10 @@
 public class Lexer {
     // List out all tokens from our predefined grammar https://www.labouseur.com/courses/compilers/grammar.pdf
     public static enum Grammar{
-
+        EOP, L_BRACE, R_BRACE, VARIABLE_TYPE, IF, WHILE,
+        PRINT, ASSIGNMENT_OP, ID, L_QUOTE, R_QUOTE, L_PARENTH,
+        R_PARENTH, CHAR, DIGIT, EQUALITY_OP, INEQUALITY_OP,
+        BOOL, ADDITION_OP
     }
     public void get_token(){
 
