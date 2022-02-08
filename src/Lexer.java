4561369f8b9fa3e4674b406865a19f7850cf9eac
MasonN808 <mason.nakamura1@marist.edu>
2022-02-08 17:57:08 -0500

    Added more cases and attempting to implement longest match and rule order

--- src/Lexer.java
+++ src/Lexer.java
@@ -20,26 +20,56 @@ public class Lexer {
 
     public ArrayList<Token> get_token_stream(String s, boolean verbose){
         ArrayList<Token> token_stream = new ArrayList<Token>(); // Initialize the token_stream which what will be given to the parser
-        int i = 0; // Initialize index
-        while(i < s.length()){
-            char current_char = s.charAt(i);
+        int current_index = 0; // Initialize index
+        int last_index;
+        boolean EOP_found = false;
+        boolean close_quote_found = true; // to check if closed quote is found TODO: check this case """" -> is it two quotes or one quote?
+        boolean L_parenth_found = false; // to check if left parenthesis is found
+        boolean R_parenth_found = false; // to check if right parenthesis is found
+        while(current_index < s.length() | EOP_found){
+            char current_char = s.charAt(current_index);
             Token token;
-            switch (current_char){
+            switch (current_char) {
                 // USE REGULAR EXPRESSIONS HERE FROM import java.util.regex.Matcher;
                 //import java.util.regex.Pattern;
-                case ('$'): token = new Token(Grammar.EOP, "$");
-                            add_token(token_stream, token, verbose); //Add token to the token stream
-                            break;
-                case ('{'): token = new Token(Grammar.L_BRACE, "{");
-                            add_token(token_stream, token, verbose);
-                            break;
-                case ('}'): token = new Token(Grammar.R_BRACE, "}");
-                            add_token(token_stream, token, verbose);
-                            break;
-                case ('"'): token = new Token(Grammar.QUOTE, "\"");
-                            add_token(token_stream, token, verbose);
-                            break;
+                // Checking easiest tokens to identify without
+                case ('$') -> {
+                    token = new Token(Grammar.EOP, "$");
+                    add_token(token_stream, token, verbose); //Add token to the token stream
+                    EOP_found = true;  // Found EOP so exit while
+                }
+                case ('{') -> {
+                    token = new Token(Grammar.L_BRACE, "{");
+                }
+                case ('}') -> {
+                    token = new Token(Grammar.R_BRACE, "}");
+                    add_token(token_stream, token, verbose);
+                }
+                case ('"') -> {
+                    token = new Token(Grammar.QUOTE, "\"");
+                    add_token(token_stream, token, verbose);
+                    close_quote_found = !close_quote_found;  // set close_quote_found to false
+                }
+                case ('+') -> {
+                    token = new Token(Grammar.ADDITION_OP, "+");
+                    add_token(token_stream, token, verbose);
+                }
+                case ('=') -> {
+                    token = new Token(Grammar.ASSIGNMENT_OP, "=");
+                    add_token(token_stream, token, verbose);
+                }
+                case ('(') -> {
+                    token = new Token(Grammar.L_PARENTH, "(");
+                    L_parenth_found = true;
+                    add_token(token_stream, token, verbose);
+                }
+                case (')') -> {
+                    token = new Token(Grammar.R_PARENTH, ")");
+                    R_parenth_found = true;
+                    add_token(token_stream, token, verbose);
+                }
             }
+            add_token(token_stream, token, verbose);
         }
         return null;
     }
