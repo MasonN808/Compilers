b06d79157777c53640bf826d21c2d79ad206f604
MasonN808 <mason.nakamura1@marist.edu>
2022-02-07 23:22:14 -0500

    Added constructor for token class

--- src/Token.java
+++ src/Token.java
@@ -2,5 +2,11 @@ public class Token {
     public Lexer.Grammar token_type;
     public String s;
     public int line_number; // For error log
-    public int charcter_number; // For error log
+    public int character_number; // For error log
+
+    // Constructor for Token Class
+    public Token(Lexer.Grammar token_type, String s){
+        this.token_type = token_type;
+        this.s = s;
+    }
 }
