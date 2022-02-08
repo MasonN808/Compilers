ff1a88699369bac3c57aa897277a2e6301f92457
MasonN808 <mason.nakamura1@marist.edu>
2022-02-07 20:59:01 -0500

    Initializing Lexer

---
+++ Compilers/src/Lexer.java
@@ -0,0 +1,9 @@
+public class Lexer {
+    // List out all tokens from our predefined grammar https://www.labouseur.com/courses/compilers/grammar.pdf
+    public static enum Grammar{
+
+    }
+    public void get_token(){
+
+    }
+}
