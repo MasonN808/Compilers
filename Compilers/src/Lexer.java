2f23550d66277ad3a2022e7b0a4336389befa964
MasonN808 <mason.nakamura1@marist.edu>
2022-02-07 21:47:25 -0500

    Created list of available tokens from our grammar

--- Compilers/src/Lexer.java
+++ Compilers/src/Lexer.java
@@ -1,7 +1,9 @@
 public class Lexer {
     // List out all tokens from our predefined grammar https://www.labouseur.com/courses/compilers/grammar.pdf
     public static enum Grammar{
-
+        EOP, LBRACE, RBRACE, PRINT_STATEMENT,  ASSIGNMENT_OP, VARIABLE_TYPE,
+        WHILE_STATEMENT, IF_STATEMENT, ID, EQUALITY_0P, INEQUALITY_OP, ADDITION_OP,
+        BOOL, DIGIT, WHILE, SPACE, LPARENTH, RPARENTH, LQUOTE, RQUOTE
     }
     public void get_token(){
 
