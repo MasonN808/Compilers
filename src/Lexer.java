6f110a51945e454910d1598a9fd07ff8fdc123af
MasonN808 <mason.nakamura1@marist.edu>
2022-02-08 18:38:25 -0500

    Added tokens that use regular expressions in default of switch{}

--- src/Lexer.java
+++ src/Lexer.java
@@ -1,24 +1,25 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Lexer {
     // List out all tokens from our predefined grammar https://www.labouseur.com/courses/compilers/grammar.pdf
-    public static enum Grammar{
+    public static enum Grammar {
         EOP, L_BRACE, R_BRACE, VARIABLE_TYPE, IF, WHILE,
         PRINT, ASSIGNMENT_OP, ID, QUOTE, L_PARENTH,
         R_PARENTH, CHAR, DIGIT, EQUALITY_OP, INEQUALITY_OP,
         BOOL, ADDITION_OP
     }
 
-    public static void add_token(ArrayList<Token> token_stream, Token token, boolean verbose){
-        if (verbose){
+    public static void add_token(ArrayList<Token> token_stream, Token token, boolean verbose) {
+        if (verbose) {
             System.out.println("Lexer -------> found " + token.token_type + " [" + token.s + "] at "
-                    + token.line_number + ", "+ token.character_number);
+                    + token.line_number + ", " + token.character_number);
         }
         token_stream.add(token);
     }
 
-    public ArrayList<Token> get_token_stream(String s, boolean verbose){
+    public ArrayList<Token> get_token_stream(String s, boolean verbose) {
         ArrayList<Token> token_stream = new ArrayList<Token>(); // Initialize the token_stream which what will be given to the parser
         int current_index = 0; // Initialize index
         int last_index;
@@ -27,7 +28,7 @@ public class Lexer {
         boolean close_quote_found = true; // to check if closed quote is found TODO: check this case """" -> is it two quotes or one quote?
         boolean L_parenth_found = false; // to check if left parenthesis is found
         boolean R_parenth_found = false; // to check if right parenthesis is found
-        while(current_index < s.length() | EOP_found){
+        while (current_index < s.length() | EOP_found) {
             char current_char = s.charAt(current_index);
             current_string = current_string + current_char; // append the current character to the string we use to find a token
             Token token; // initialize the token
@@ -35,7 +36,7 @@ public class Lexer {
                 // USE REGULAR EXPRESSIONS HERE FROM import java.util.regex.Matcher;
                 //import java.util.regex.Pattern;
                 // Checking easiest tokens to identify without
-                case ("$") -> {
+                case ("$") -> { // TODO: maybe change the String s in Token() to current_string to be more general
                     token = new Token(Grammar.EOP, "$"); // END OF PROGRAM (EOP)
                     add_token(token_stream, token, verbose); //Add token to the token stream
                     EOP_found = true;  // Found EOP so exit while
@@ -111,9 +112,23 @@ public class Lexer {
                     token = new Token(Grammar.VARIABLE_TYPE, "boolean");
                     add_token(token_stream, token, verbose);
                 }
-            }
+                default -> { //since you can't use regex in switch{}, we put the many cases in default
+                    if (current_string.matches("[a-z]") & !close_quote_found) { // It is a CHAR if in quotes, else ID
+                        token = new Token(Grammar.CHAR, current_string); // CHAR
+                        add_token(token_stream, token, verbose);
+                    }
+                    if (current_string.matches("[a-z]")) {
+                        token = new Token(Grammar.ID, current_string); // CHAR
+                        add_token(token_stream, token, verbose);
+                    }
+                    if (current_string.matches("[0-9]")) {
+                        token = new Token(Grammar.DIGIT, current_string); // CHAR
+                        add_token(token_stream, token, verbose);
+                    }
 
-        }
+                }
+            } // end switch
+        } // end while
         return null;
-    }
-}
+    } // end get_token_stream
+} // end class
