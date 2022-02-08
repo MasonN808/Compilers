20ecb91a1e6d9065acfd3de2e7d9c77004e3b866
MasonN808 <mason.nakamura1@marist.edu>
2022-02-08 00:39:21 -0500

    -Added add_token for verbose effect and to make codebase neater -Added more cases for lex and still need to get even MORE with complexity -Changed from L_QUOTE and R_QUOTE to just QUOTE (might change later)

--- src/Lexer.java
+++ src/Lexer.java
@@ -6,20 +6,37 @@ public class Lexer {
     // List out all tokens from our predefined grammar https://www.labouseur.com/courses/compilers/grammar.pdf
     public static enum Grammar{
         EOP, L_BRACE, R_BRACE, VARIABLE_TYPE, IF, WHILE,
-        PRINT, ASSIGNMENT_OP, ID, L_QUOTE, R_QUOTE, L_PARENTH,
+        PRINT, ASSIGNMENT_OP, ID, QUOTE, L_PARENTH,
         R_PARENTH, CHAR, DIGIT, EQUALITY_OP, INEQUALITY_OP,
         BOOL, ADDITION_OP
     }
 
-    public ArrayList<Token> get_token_stream(String s){
+    public static void add_token(ArrayList<Token> token_stream, Token token, boolean verbose){
+        if (verbose){
+            System.out.println("Lexer -------> found " + token.token_type + " [" + token.s + "] at "
+                    + token.line_number + ", "+ token.character_number);
+        }
+        token_stream.add(token);
+    }
+
+    public ArrayList<Token> get_token_stream(String s, boolean verbose){
         ArrayList<Token> token_stream = new ArrayList<Token>(); // Initialize the token_stream which what will be given to the parser
         int i = 0; // Initialize index
         while(i < s.length()){
             char current_char = s.charAt(i);
+            Token token;
             switch (current_char){
-                case ('$'): Token token = new Token(Grammar.EOP, "$");
-                            token_stream = Arrays.copyOf(token_stream, token_stream.length + 1); //extends memory
-                            token_stream.
+                case ('$'): token = new Token(Grammar.EOP, "$");
+                            add_token(token_stream, token, verbose); //Add token to the token stream
+                            break;
+                case ('{'): token = new Token(Grammar.L_BRACE, "{");
+                            add_token(token_stream, token, verbose);
+                            break;
+                case ('}'): token = new Token(Grammar.R_BRACE, "}");
+                            add_token(token_stream, token, verbose);
+                            break;
+                case ('"'): token = new Token(Grammar.QUOTE, "\"");
+                            add_token(token_stream, token, verbose);
                             break;
             }
         }
