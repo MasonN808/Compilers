4146908eb8b56d2f6dc97fb47e18532f659f664a
MasonN808 <mason.nakamura1@marist.edu>
2022-03-12 15:17:58 -0500

    -Added CST String output for Tree class

--- src/Tree.java
+++ src/Tree.java
@@ -28,7 +28,45 @@ public class Tree {
         }
     }
 
-    public static void moveUp(){
+    public static void moveUp() {
         current = current.parent; //Go up the tree
+    }
+
+
+    // Recursive function to handle the expansion of the nodes.
+    public static void expand(Node node, int depth, String traversalResult){
+        // Space out based on the current depth so
+        // this looks at least a little tree-like.
+        for (int i = 0; i < depth; i++)
+        {
+            traversalResult = traversalResult.concat("-");
+        }
+
+        // If there are no children (i.e., leaf nodes)...
+        if (node.children.isEmpty()) {
+            System.out.println("CHILDREN IS EMPTY");
+            // ... note the leaf node.
+            traversalResult = traversalResult.concat("[" + node.name + "] \n");
+        }
+
+        else
+        {
+            // There are children, so note these interior/branch nodes and ...
+            traversalResult = traversalResult.concat("<" + node.name + "> \n");
+            // .. recursively expand them.
+            for (var i = 0; i < node.children.size(); i++)
+            {
+                expand(node.children.get(i), depth + 1, traversalResult);
+            }
+        }
+    }
+
+    public static String cstToString() {
+        // Initialize the result string.
+        var traversalResult = "";
+        // Make the initial call to expand from the root.
+        expand(root, 0, traversalResult);
+        // Return the result.
+        return traversalResult;
     }
 }
