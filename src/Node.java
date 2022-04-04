f30c39d3241fcfe9dbb2415bf24f4eed65cc07cf
Mason Nakamura <Mason.Nakamura1@marist.edu>
2022-04-04 10:21:43 -0400

    Added isBlock (but may not need it if we just use children as an indicator)

--- src/Node.java
+++ src/Node.java
@@ -16,6 +16,8 @@ public class Node {
     public String value = null; //value of the token_type, if it exists
     public ArrayList<Node> children = new ArrayList<>(); //children of current node
     public String kind = null; // for making CST
+    // For AST
+    public boolean isBlock = false;
 
 //    public Node(Node parent, String name, String value, ArrayList<Node> children){
 //        this.parent = parent;
