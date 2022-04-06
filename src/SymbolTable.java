3efc3c5def75fd4a3f37dcae43a8c52b394dbd87
MasonN808 <mason.nakamura1@marist.edu>
2022-04-06 18:00:05 -0400

    - Edits to SymbolTable - Initialized Node class and Tree class for symbol table specifically

--- src/SymbolTable.java
+++ src/SymbolTable.java
@@ -1,2 +1,11 @@
 public class SymbolTable {
+    TreeAST ast = null;
+
+    public SymbolTable(TreeAST ast){
+        this.ast = ast; // initialize AST that our symbol table will use
+    }
+
+    public static void makeTable(){
+
+    }
 }
