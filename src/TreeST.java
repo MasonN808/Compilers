a18483edee0eeb5a879c466e2a32530bf88091d8
MasonN808 <mason.nakamura1@marist.edu>
2022-04-06 21:02:37 -0400

    - Getting symbol table tree to work

--- src/TreeST.java
+++ src/TreeST.java
@@ -12,15 +12,31 @@ public class TreeST {
     }
 
     public static void addNode(String id, String type, boolean isInitialized, boolean isUsed){
-        Node node = new Node();
+        Node node = new Node(); //initialize new Node
         idDetails details = new idDetails(type, current.token, isInitialized, isUsed);
         Hashtable<String,idDetails> ht = new Hashtable<>();
-        ht.put(id, details);
+        idDetails a = ht.put(id, details);
+
+        if (root == null) { //case for the first node in tree
+            root = node;
+            node.parent = null;
+        } else {
+            node.parent = current; //Assign n's parent to current node
+
+            node.parent.children.add(node); //add n to parent's children array, letting parent know who its children is
+        }
+
     }
 
     public static void populateST(Node node){ //start will be AbstractSyntaxTree.ast.root
+        if (root == null){
+            root = node;
+            current = root;
+        }
         for (Node each : node.children) {
-
+            if(each.name.equals("varDecal")){
+                addNode();
+            }
             populateST(each);
         }
     }
