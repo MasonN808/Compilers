4cf88ab3ef68d50124d31b53490b6295e2f8d623
MasonN808 <mason.nakamura1@marist.edu>
2022-04-06 20:46:33 -0400

    - Created hashtable attribute - Changed token from type Compiler808.Grammar to Token type

--- src/Node.java
+++ src/Node.java
@@ -1,5 +1,7 @@
 import java.sql.Array;
 import java.util.ArrayList;
+import java.util.Hashtable;
+
 /**
  * <h1>Node Class</h1>
  * We use this Node class to construct our Concrete Syntax Tree in the Trees Class.
@@ -16,9 +18,9 @@ public class Node {
     public String value = null; //value of the token_type, if it exists
     public ArrayList<Node> children = new ArrayList<>(); //children of current node
     public String kind = null; // for making CST
-    public Compiler808.Grammar token = null;
+    public Token token = null;
     // For AST
-    public boolean isBlock = false;
+    public Hashtable<String,idDetails> ht = null;
 
 //    public Node(Node parent, String name, String value, ArrayList<Node> children){
 //        this.parent = parent;
