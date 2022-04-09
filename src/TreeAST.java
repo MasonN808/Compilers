c4edbd5147f8811559ddcacb703b119224a35f71
MasonN808 <mason.nakamura1@marist.edu>
2022-04-08 23:48:42 -0400

    - Edits to print of booleanExpr

--- src/TreeAST.java
+++ src/TreeAST.java
@@ -76,7 +76,7 @@ public class TreeAST {
         } else {
             if (node.name.equals("boolOp")){
 //                System.out.println(traversalResult.concat("<" + node.name + ", " + node.value + " >"));
-                System.out.println(traversalResult.concat("<" + node.value + " >"));
+                System.out.println(traversalResult.concat("<" + node.value + ">"));
 
             }
             else{
