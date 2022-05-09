ddc784c18a5ae2b915a0a2ce4227f63551d262de
Mason Nakamura <Mason.Nakamura1@marist.edu>
2022-05-09 13:28:55 -0400

    Added more methods to implement and created new pointers for the index

--- src/CodeGen.java
+++ src/CodeGen.java
@@ -2,13 +2,14 @@ public class CodeGen {
     public static String[] opsArray = null; // Might make into matrix
     public static TreeST symbolTable = null;
     public static TreeAST ast = null;
+    public static int curIndex = 0;
 
     public static int numErrors = 0; // Keep track of errors in code gen
 
     public CodeGen(TreeST symbolTable, TreeAST ast){
         // Reset opsArray to empty string of certain length
         this.opsArray = new String[256]; // TODO: make sure 256 is the right length and not 255
-        this.symbolTable = symbolTable;
+        this.symbolTable = symbolTable; // Might not actually need the symbol table since semantic completed successfully (prereq)
         this.ast = ast;
     }
 
@@ -22,5 +23,17 @@ public class CodeGen {
                 -Assign particular opt code for certain nodes
          */
     }
+
+    public static String[][] arrayToMatrix(){
+        // TODO: make this
+        return null;
+    }
+
+    public static String[] replaceTemp(){
+        // TODO: make this
+        return null;
+    }
+
+
 
 }
