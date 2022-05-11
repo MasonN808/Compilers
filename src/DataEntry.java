1bf195f50874e288e614e7dab8e321c482d68370
MasonN808 <mason.nakamura1@marist.edu>
2022-05-11 13:38:08 -0400

    -Initialized for entries in static data arraylist

---
+++ src/DataEntry.java
@@ -0,0 +1,16 @@
+/**
+ * <h1>DataEntry Class</h1>
+ * We use this DataEntry class to construct our Concrete Syntax Tree in the Trees Class.
+ * Part of Parser (Project 2)
+ * <p>
+ * <b>Note:</b> Still in progress
+ *
+ * @author  Mason Nakamura
+ * @since   03-04-2022
+ */
+public class DataEntry {
+    public String temp = null;
+    public String var = null;
+    public int scope = 0;
+    public int offset = 0;
+}
