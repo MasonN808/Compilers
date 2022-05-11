d658dd23e60a5cb356b89470758ddb9676176cd7
MasonN808 <mason.nakamura1@marist.edu>
2022-05-11 13:38:35 -0400

    -Added value attribute to idDetails (not sure if we need it later on)

--- src/idDetails.java
+++ src/idDetails.java
@@ -4,12 +4,14 @@ public class idDetails {
     Token token = null;
     boolean isInitialized = false;
     boolean isUsed = false;
+    String value = null;
     int depth = 0;
 
-    public idDetails(String type, boolean isInitialized, boolean isUsed, Token token){
+    public idDetails(String type, boolean isInitialized, boolean isUsed, Token token, String value){
         this.type = type;
         this.token = token;
         this.isInitialized = isInitialized;
         this.isUsed = isUsed;
+        this.value = value;
     }
 }
